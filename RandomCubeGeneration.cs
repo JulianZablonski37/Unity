f2e31ed4e5a53597d7c431f07cf1985c2f7378c2
JulianZablonski37 <79467795+JulianZablonski37@users.noreply.github.com>
2021-11-05 10:40:05 +0100

    Add files via upload
    zadani1 z lab 4

---
+++ RandomCubeGeneration.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class RandomCubeGeneration : MonoBehaviour
+{
+    List<Vector3> positions = new List<Vector3>();
+    public int numberOfObject = 40;
+    public float delay = 3.0f;
+    int objectCounter = 0;
+    // obiekt do generowania
+    public GameObject block;
+    public Material[] materialList;
+    
+
+    void Start()
+    {
+        Vector3 planeBounds = GetComponent<Collider>().bounds.size;
+        // w momecie uruchomienia generuje 10 kostek w losowych miejscach
+        List<(int, int)> Cords = new List<(int, int)>();
+        Collider da32w = gameObject.GetComponent<Collider>();
+        int x;
+        int z;
+        int tmp = numberOfObject;
+        while (tmp > 0)
+        {
+            x = (int)UnityEngine.Random.Range(da32w.bounds.center.x - (planeBounds.x / 2), da32w.bounds.center.x + (planeBounds.x / 2));
+            z = (int)UnityEngine.Random.Range(da32w.bounds.center.z - (planeBounds.z / 2), da32w.bounds.center.z+ (planeBounds.z/2));
+            if (!Cords.Contains((x, z)))
+            {
+                Cords.Add((x, z));
+                tmp--;
+            }
+        }
+
+        for (int i = 0; i < numberOfObject; i++)
+        {
+            this.positions.Add(new Vector3(Cords[i].Item1, 5, Cords[i].Item2));
+        }
+        foreach (Vector3 elem in positions)
+        {
+            Debug.Log(elem);
+        }
+   
+        // uruchamiamy coroutine
+        StartCoroutine(GenerujObiekt());
+    }
+
+    void Update()
+    {
+
+    }
+
+    IEnumerator GenerujObiekt()
+    {
+        Debug.Log("wywo³ano coroutine");
+        foreach (Vector3 pos in positions)
+        {
+            this.block.GetComponent<Renderer>().material = materialList[UnityEngine.Random.Range(0,materialList.Count())];
+            Instantiate(this.block, this.positions.ElementAt(this.objectCounter++), Quaternion.identity);
+            yield return new WaitForSeconds(this.delay);
+        }
+        // zatrzymujemy coroutine
+        StopCoroutine(GenerujObiekt());
+    }
+}
